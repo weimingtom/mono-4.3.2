56126d8ab6f245c876c24ae1ae880df27db02522
ClarkGuan <guangongbo@didichuxing.com>
2016-04-17 01:37:04 +0800

    fix "undefined reference to 'mono_threads_core_get_stack_bounds'" error

--- mono-4.3.2/mono/utils/mono-threads-linux.c
+++ mono-4.3.2/mono/utils/mono-threads-linux.c
@@ -1,6 +1,7 @@
 #include <config.h>
 
-#if (defined(__linux__) && !defined(PLATFORM_ANDROID)) || defined(__FreeBSD_kernel__)
+//#if (defined(__linux__) && !defined(PLATFORM_ANDROID)) || defined(__FreeBSD_kernel__)
+#if defined(__linux__)
 
 #include <mono/utils/mono-threads.h>
 #include <pthread.h>
